9db8e0018a87f5e709985810b3966f081b4cdab1
Tran Kim Truong <truongprobk1992@gmail.com>
2015-07-07 10:49:04 +0700

    end task 2

--- client/index.js
+++ client/index.js
@@ -1,9 +1,17 @@
 // This code only runs on the client
 Meteor.subscribe("sentences");
+Meteor.subscribe("translate");
 
 Template.body.helpers({
   Sentences: function () {
       return Sentences.find({}, {sort: {createdAt: -1}});
+  },
+  Translate: function () {
+      return Translate.find({}, {sort: {createdAt: -1}});
+  },
+  isTranslate: function () {
+    if (Session.get("sentenceSelected")===undefined) return false;
+    return true;
   }
 });
 
@@ -17,6 +25,12 @@ Template.body.events({
     // Clear form
     event.target.text.value = "";
     // Prevent default form submit
+    return false;
+  },
+  "submit .translate-a-sentence": function (event) {
+    var text = event.target.text.value;
+    Meteor.call("addTranslate",Session.get("sentenceSelected"),text);
+    event.target.text.value = "";
     return false;
   }
 });
@@ -33,5 +47,17 @@ Template.sentences.events({
   },
   "click li": function () {
     Session.set("sentenceSelected",this._id);
+  }
+});
+
+Template.translate_sentence.helpers({
+  translateSentence: function () {
+    return Sentences.find({_id: Session.get("sentenceSelected")}).fetch()[0].text;
+  }
+});
+
+Template.show_translate.helpers({
+  showSentence: function () {
+    return Sentences.find({_id: this.sentenceid}).fetch()[0].text;
   }
 });
--- server/publications.js
+++ server/publications.js
@@ -3,3 +3,8 @@ Meteor.publish("sentences", function () {
   return Sentences.find({
   });
 });
+
+Meteor.publish("translate", function () {
+  return Translate.find({
+  });
+});
